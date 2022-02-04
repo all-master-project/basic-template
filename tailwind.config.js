13be27ad252a4ce9776971abf18972231c434b8a
Brahim <elbanaji.brahim2@gmail.com>
2022-02-04 15:08:38 +0100

    gooooooood Admin dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,84 +1,26 @@
 const plugin = require("tailwindcss/plugin");
 const colors = require("tailwindcss/colors");
-const homecolors = {
-  white: "#ffffff",
-  black: "#38424D",
-  gray: "#FBFBFF",
-  yellow: "#FDD446",
-  "theme-color": "#F94F4F",
-  "border-color": "#E8E8E8",
-  "body-color": "#747E88",
-  "heading-color": "#162447",
-  shadow: "#9D6A6A",
-};
+
 module.exports = {
   purge: {
-    enable: true,
+    enabled: true,
     content: [
       "./**/*.html",
-      "./assets/js/main.js",
       "./*.html",
       "./**/*.js",
       "./*.js",
       "./**/*.ts",
       "./*.ts",
     ],
+    options: {
+      safelist: [],
+    },
   },
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      sm: "540px",
-      md: "720px",
-      lg: "960px",
-      xl: "1140px",
-      "2xl": "1320px",
+    colors: {
+      ...colors,
     },
-    container: {
-      center: true,
-    },
-
-    spacing: {
-      0: "0px",
-      1: "4px",
-      2: "8px",
-      3: "12px",
-      4: "16px",
-      5: "20px",
-      6: "24px",
-      7: "28px",
-      8: "32px",
-      9: "36px",
-      10: "40px",
-      11: "44px",
-      12: "48px",
-      13: "52px",
-      14: "56px",
-      15: "60px",
-      16: "64px",
-      17: "68px",
-      18: "72px",
-      19: "76px",
-      20: "80px",
-      21: "84px",
-      22: "88px",
-      23: "92px",
-      24: "96px",
-      25: "100px",
-      110: "110px",
-      120: "120px",
-      130: "130px",
-      140: "140px",
-      150: "150px",
-      160: "160px",
-      170: "170px",
-      180: "180px",
-      190: "190px",
-      200: "200px",
-      260: "260px",
-    },
-    colors: { ...colors },
     extend: {
-      homecolors,
       minHeight: {
         "screen-75": "75vh",
       },
@@ -91,14 +33,6 @@ module.exports = {
       zIndex: {
         2: 2,
         3: 3,
-        "-10": "-10",
-        "-20": "-20",
-        "-30": "-30",
-        "-40": "-40",
-      },
-      transitionDelay: {
-        400: "400ms",
-        600: "600ms",
       },
       inset: {
         "-100": "-100%",
